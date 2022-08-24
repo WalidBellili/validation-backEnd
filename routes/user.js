d2297912934240ed0458bc0613961368c8ca50b7
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-24 17:21:11 +0200

    user route with slug done tested with postman

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const users = require("../users.json");
+const { checkIfExists } = require("../middlewares/user");
 
+// une route qui renvoie tous les users
 app.get("/", (req, res) => {
   res.json(users);
+});
+
+// une route qui pour 1 user grace au slug
+
+app.get("/:slug", checkIfExists, (req, res) => {
+  console.log(req.user);
+});
+
+// Une route qui crée un user
+
+app.post("/new-user", (req, res) => {
+  console.log("ok");
 });
 
 module.exports = app;
