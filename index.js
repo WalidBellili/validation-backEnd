5e961412f8652ad0bc71ea51eb24423eb363896e
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-24 21:12:27 +0200

    import cors forgotten

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const morgan = require("morgan");
+const cors = require("cors");
 const usersRoutes = require("./routes/user");
+const { ContextRunnerImpl } = require("express-validator/src/chain");
 
+app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.json());
-
 app.use("/users", usersRoutes);
 
 app.listen(port, () => {
